a4c6612c25df77dc7142234c0e0aaa27f2b3536a
pathi14 <pathipayong@gmail.com>
2025-03-05 22:26:42 +0100

    feat: initialisation des endpoints liés à l'utilisateur

--- cmd/api/run.go
+++ cmd/api/run.go
@@ -3,11 +3,21 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/pathi14/AuthentificationGO/internal"
+	"github.com/pathi14/AuthentificationGO/internal/user"
 )
 
 func Run() {
 	router := gin.Default()
+
+	//routes accessibles à tous
 	router.GET("/health", internal.Health)
+	router.POST("/register", user.Register)
+	router.POST("/login", user.Login)
+	router.POST("/reset-password", user.UpdatePassword)
+
+	//routes protégées
+	router.POST("/logout", user.Logout)
+	router.GET("/profile", user.Profile)
 
 	router.Run("localhost:8080")
 }
